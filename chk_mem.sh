44a1e0f0fa0d104f5cd0e3561aa90782c5290295
hctseng04 <87069670+hctseng04@users.noreply.github.com>
2024-05-27 15:05:00 +0800

    Create chk_mem.sh

---
+++ chk_mem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERS_LIST=`ps aux | awk '{print $1}' | sort | uniq`
+EXCLUDED_LIST="USER avahi chrony colord dbus dnsmasq geoclue libstor+ node_ex+ polkitd postfix rpc rtkit"
+
+for skip_user in $EXCLUDED_LIST; do
+    USERS_LIST=`echo $USERS_LIST | sed "s/$skip_user//g"`
+done
+
+(echo -e "USER RSS VMEM\n-------- -------- --------";
+ for user in $USERS_LIST; do
+   echo $user $(ps -U $user --no-headers -o rss,vsz \
+     | awk '{rss+=$1; vmem+=$2} END{
+           cmdrss=sprintf("numfmt --from-unit=K --to=iec --format %s %d", "%8.1f", rss);
+           cmdrss | getline converted_rss;
+           close(cmdrss);
+           cmdvmem=sprintf("numfmt --from-unit=K --to=iec --format %s %d", "%8.1f", vmem);
+           cmdvmem | getline converted_vmem;
+           print converted_rss" "converted_vmem
+       }')
+ done | sort -k3 -h
+) | column -t -R2,3
