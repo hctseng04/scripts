235da07e07c2dbb9fe564912b3be7e67bc8a1823
hctseng04 <87069670+hctseng04@users.noreply.github.com>
2024-06-12 15:10:05 +0800

    Update chk_mem.sh

--- chk_mem.sh
+++ chk_mem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERS_LIST=`ps aux | awk '{print $1}' | sort | uniq`
-EXCLUDED_LIST="USER avahi chrony colord dbus dnsmasq geoclue libstor+ node_ex+ polkitd postfix rpc rtkit"
+EXCLUDED_LIST="USER avahi chrony colord dbus dhcpd dnsmasq geoclue libstor+ node_ex+ polkitd postfix rpcuser rpc rtkit smmsp"
 
 for skip_user in $EXCLUDED_LIST; do
     USERS_LIST=`echo $USERS_LIST | sed "s/$skip_user//g"`
@@ -9,7 +9,12 @@ done
 
 (echo -e "USER RSS VMEM\n-------- -------- --------";
  for user in $USERS_LIST; do
-   echo $user $(ps -U $user --no-headers -o rss,vsz \
+   if [[ $user == *"+" ]]; then
+       user_fullname=`id -nu \`cat /proc/\\\`ps aux | grep $user | grep -v grep | awk '{print $2}' | head -n 1\\\`/status | grep Uid: | awk '{print $2}'\``
+   else
+       user_fullname=$user
+   fi
+   echo $user $(ps -U $user_fullname --no-headers -o rss,vsz \
      | awk '{rss+=$1; vmem+=$2} END{
            cmdrss=sprintf("numfmt --from-unit=K --to=iec --format %s %d", "%8.1f", rss);
            cmdrss | getline converted_rss;
